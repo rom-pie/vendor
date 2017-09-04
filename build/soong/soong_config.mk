284cb938e73a8d528603b1c30b40c38f505c7439
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: Put aosmp variables in the struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Lineage structure
 aosmp_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Aosmp": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
