31bde0f34f8fab3995b6da0131c0bdde56cf4693
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-17 09:20:21 -0700

    build: config: Ignore neverallows
    Change-Id: I35a39ec73c0047f93124f72249a6fa936fe0875a
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- build/core/config.mk
+++ build/core/config.mk
@@ -3,3 +3,7 @@ include $(TOPDIR)vendor/aosmp/build/core/mtk_target.mk
 
 # Rules for QCOM targets
 include $(TOPDIR)vendor/aosmp/build/core/qcom_target.mk
+
+# We modify several neverallows, so let the build proceed
+SELINUX_IGNORE_NEVERALLOWS := true
+
