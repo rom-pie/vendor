95e1d00a60a5b17c700b29158812c99ccaec72c0
Luca Stefani <luca.stefani.ge1@gmail.com>
2019-01-05 11:46:23 +0100

    vendor: config: Disable art debug and enable minimize debug info
    Change-Id: I12b65c532201bf31f12cfb1d62c4a8bbe2204e2f
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -51,6 +51,14 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/aosmp/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
+# Do not include art debug targets
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
 # Packages
 include vendor/aosmp/config/packages.mk
 
