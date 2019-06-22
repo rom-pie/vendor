f104a6a4429d7ef5c03a46afc9e9b6e2199865a6
Ethan Chen <intervigil@gmail.com>
2019-06-22 14:12:40 -0700

    vendor: build: Separate platform macros from QCOM platform definition
    * The macros used in the file formerly called "qcom_utils.mk" are
      actually generic and not QCOM-specific. Move the QCOM_BOARD definitions
      to a QCOM-specific location, and name the remaning macros something more
      generic.
    
    Change-Id: I6747bc9201e56b456caf251122057d7dbc233f1c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -1,6 +1,9 @@
 define uniq
 $(if $1,$(firstword $1) $(call uniq,$(filter-out $(firstword $1),$1)))
 endef
+ 
+# Include board/platform macros
+include vendor/aosmp/build/core/utils.mk
 
-# Include QCOM macros
-include vendor/aosip/build/core/qcom_utils.mk
+# Include vendor platform definitions
+include vendor/aosmp/build/core/vendor/*.mk
