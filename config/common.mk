ArrowOS/android_vendor_arrow

vendor: Add Pixel's sysconfigs
Change-Id: I17fb14b9cdad71aa39986b799f062e202daaacad

config/common.mk
@@ -55,6 +55,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/arrow/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
 
+
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/arrow/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
+
 # Bootanimation
 include vendor/arrow/config/bootanimation.mk
 

rom-pie/vendor

Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

config/common.mk
@@ -65,6 +65,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/aosmp/overlay/dictionaries
 PRODUCT_COPY_FILES += \
     vendor/aosmp/config/permissions/aosmp-power-whitelist.xml:system/etc/sysconfig/aosmp-power-whitelist.xml
 
+# Pixel sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/aosmp/prebuilt/common/etc/sysconfig/pixel.xml:system/etc/sysconfig/pixel.xml
+
 # Sensitive Phone Numbers list
 PRODUCT_COPY_FILES += \
     vendor/aosmp/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml