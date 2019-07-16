00e61c4617d5df8a90a6253b4ade2b262c81aedb
calebcabob <calphonic@gmail.com>
2019-07-15 21:47:38 -0400

    base: SwitchStyler: Drop half the switch styles. We don't need all these.
    Change-Id: I19a94d5ee518937c10b135edfb834013a9cbaca5
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Sebastian <kjjjnob.seba@gmail.com>

--- config/themes.mk
+++ config/themes.mk
@@ -31,12 +31,7 @@ PRODUCT_PACKAGES += \
     MD2Switch \
     OnePlusSwitch \
     StockSwitch \
-    SwitchOne \
-    SwitchTwo \
-    SwitchThree \
-    SwitchFour \
-    SwitchFive \
-    SwitchSix \
-    SwitchSeven \
-    SwitchEight \
-    SwitchNine
+    Contained \
+    Retro \
+    Stockish \
+    Narrow
